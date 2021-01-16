b110ce1584c6575225c298e7680409e9cbd65bd7
thoratica <me@tica.fun>
2021-01-16 11:29:47 +0900

    Add SCSS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { json, urlencoded } = require('body-parser');
 const { resolve } = require('path');
+const sassMiddleware = require('node-sass-middleware');
 const {} = require('node-kakao');
 const { main } = require('./routes');
 
@@ -9,6 +10,16 @@ const port = process.env.PORT || 3000;
 
 app.set('views', resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
+app.use(
+  sassMiddleware({
+    src: resolve(__dirname, './scss'),
+    dest: resolve(__dirname, './public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css',
+  })
+);
+app.use(express.static(resolve(__dirname, './public')));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use('/', main);
--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.render('index');
 });
 
 module.exports = router;
